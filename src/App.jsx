398c3a8f8329a8b37eacb2f50e243ca41eff6f8c
Elamir <engineeralamiir@gmail.com>
2022-04-24 07:07:39 +0200

    Change File's Name

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Menu from "./components/Menu";
+import ProjectPage from "./mainComponents/projectsPageComponent/ProjectPage";
 
 const Home = React.lazy(() => import("./mainComponents/Home"));
-const Story = React.lazy(() => import("./mainComponents/storyPageComponent/Story"));
+const Story = React.lazy(() => import("./mainComponents/storyPageComponent/StoryPage"));
 
 const App = () => {
   return (
     <React.Suspense fallback={<p>Loading ...</p>}>
       <div>
         {/* <Home /> */}
-        <Story />
+        {/* <Story /> */}
+        <ProjectPage />
         <Menu />
       </div>
     </React.Suspense>
