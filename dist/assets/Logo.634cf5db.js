392f27c97dabac80bdd694d5369863ffb23eef67
al-amiir <engineeralamiir@gmail.com>
2022-05-10 21:33:26 +0200

    Build

---
+++ dist/assets/Logo.634cf5db.js
@@ -0,0 +1,1 @@
+import{a as o,L as r}from"./index.5f1c9b73.js";const s=()=>o(r,{to:"/",children:o("span",{className:"logo",children:"portfolio"})});export{s as L};
--- dist/assets/Logo.6e88cb66.js
+++
@@ -1,1 +0,0 @@
-import{a as o,L as r}from"./index.2e43c66c.js";const s=()=>o(r,{to:"/",children:o("span",{className:"logo",children:"portfolio"})});export{s as L};
